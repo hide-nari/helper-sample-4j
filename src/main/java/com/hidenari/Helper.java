41e15d6078d46aa1176371d9b99287d0f9cdd48d
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-09 22:20:27 +0900

    Mod method return value

--- src/main/java/com/hidenari/Helper.java
+++ src/main/java/com/hidenari/Helper.java
@@ -15,7 +15,7 @@ public class Helper {
         return fizzBuzz((int) number);
     }
 
-    public String fizzBuzz(boolean number) {
+    public void fizzBuzz(boolean number) {
         throw new IllegalArgumentException("boolean input invalid");
     }
 }
