84f743b1b2886befde3dec88a10abad38fd63d68
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-24 16:05:30 +0900

    Code clean

--- src/main/java/com/hidenari/Main.java
+++ src/main/java/com/hidenari/Main.java
@@ -1,12 +1,8 @@
 package com.hidenari;
 
-import java.util.List;
 
 public class Main {
     static void main() {
         System.out.println(new Helper<Integer>().fizzBuzz(3));
-        System.out.println(new Helper<Integer>().fizzBuzz(15));
-        System.out.println(new Helper<Integer>().fizzBuzz(4));
-        System.out.println(new Helper<Double>().fizzBuzz(3.0));
     }
 }
