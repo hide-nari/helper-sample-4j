77154709e0c0a54d832d8d138668f470e428181c
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-24 14:29:41 +0900

    Replace program sample add

--- src/main/java/com/hidenari/HelperCustom.java
+++ src/main/java/com/hidenari/HelperCustom.java
@@ -6,4 +6,10 @@ public class HelperCustom<T> extends Helper<T> {
         String result = super.fizzBuzz(number);
         return Integer.parseInt(result) % 30 == 0 ? result.toUpperCase() : result;
     }
+
+//    @Override
+//    public String fizzBuzz(int number) {
+//        var result = super.fizzBuzz(number);
+//        return number % 30 == 0 ? result.toUpperCase() : result;
+//    }
 }
--- src/main/java/com/hidenari/HelperInterface.java
+++ src/main/java/com/hidenari/HelperInterface.java
@@ -18,4 +18,22 @@ interface HelperInterface<T> {
         }
         return result.isBlank() ? String.valueOf(number) : result;
     }
+
+//    default String fizzBuzz(int number) {
+//        var result = number % 3 == 0 ? "fizz" : "";
+//        result += number % 5 == 0 ? "buzz" : "";
+//        return result.isBlank() ? String.valueOf(number) : result;
+//    }
+//
+//    default String fizzBuzz(String number) {
+//        return fizzBuzz(Integer.parseInt(number));
+//    }
+//
+//    default String fizzBuzz(double number) {
+//        return fizzBuzz((int) number);
+//    }
+//
+//    default void fizzBuzz(boolean number) {
+//        throw new IllegalArgumentException("boolean input invalid:" + number);
+//    }
 }
