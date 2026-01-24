051ee78ce644a2a51b5a1ef9bf4bebc83c8ab217
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-24 15:31:19 +0900

    Mod interface business logic

--- src/main/java/com/hidenari/HelperInterface.java
+++ src/main/java/com/hidenari/HelperInterface.java
@@ -2,27 +2,23 @@ package com.hidenari;
 
 interface HelperInterface<T> {
     default String fizzBuzz(T number) {
-        String result = "";
-        switch (number) {
-            case Boolean value -> throw new IllegalArgumentException("boolean input invalid:" + value);
+        String result = switch (number) {
+            case Integer value -> makeResultString(value);
+            case Double value -> makeResultString(value.intValue());
             case String value -> throw new IllegalArgumentException("string input invalid:" + value);
-            case Integer value -> {
-                result = value % 3 == 0 ? "fizz" : "";
-                result += value % 5 == 0 ? "buzz" : "";
-            }
-            case Double value -> {
-                result = value % 3 == 0 ? "fizz" : "";
-                result += value % 5 == 0 ? "buzz" : "";
-            }
+            case Boolean value -> throw new IllegalArgumentException("boolean input invalid:" + value);
             case null, default -> throw new IllegalArgumentException("input invalid null or default" + number);
-        }
-        return result.isBlank() ? String.valueOf(number) : result;
+        };
+        return result.isBlank() ? number.toString() : result;
     }
 
+    private String makeResultString(int value) {
+        return (value % 3 == 0 ? "fizz" : "") + (value % 5 == 0 ? "buzz" : "");
+    }
 //    default String fizzBuzz(int number) {
 //        var result = number % 3 == 0 ? "fizz" : "";
 //        result += number % 5 == 0 ? "buzz" : "";
-//        return result.isBlank() ? String.valueOf(number) : result;
+//        return result.isBlank() ? number.toString() : result;
 //    }
 //
 //    default String fizzBuzz(String number) {
--- src/main/java/com/hidenari/Main.java
+++ src/main/java/com/hidenari/Main.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 public class Main {
     static void main() {
+        System.out.println(new Helper<Integer>().fizzBuzz(3));
         System.out.println(new Helper<Integer>().fizzBuzz(15));
-        System.out.println(new Helper<Integer>().fizzBuzz(2));
+        System.out.println(new Helper<Integer>().fizzBuzz(4));
         System.out.println(new Helper<Double>().fizzBuzz(3.0));
     }
 }
