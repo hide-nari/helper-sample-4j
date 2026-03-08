1a9038ff4b0d27ea991be027b199d35ba9586b51
watanabeHi <hide3@mild.ocn.ne.jp>
2026-03-09 06:04:24 +0900

    Mod comment

--- src/test/java/com/hidenari/HelperTest.java
+++ src/test/java/com/hidenari/HelperTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName("fizzBuzz custom function")
+@DisplayName("fizzBuzz")
 class HelperTest {
 
     @Nested
