8d4bb897d8cfbeb9dd03e4efd40200cd679b7686
hide-nari <hide3@mild.ocn.ne.jp>
2026-07-18 07:53:17 +0900

    Add main function code

--- src/main/java/com/hidenari/Main.java
+++ src/main/java/com/hidenari/Main.java
@@ -3,6 +3,8 @@ package com.hidenari;
 
 public class Main {
     static void main() {
-        
+        for (int i = 0; i < 16; i++) {
+            System.out.println(new Helper<>().fizzBuzz(i));
+        }
     }
 }
